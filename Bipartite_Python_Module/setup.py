9eab90e17e4d6db4fe1fc3fbaf8943cac5a3b31e
Thiago Santos <cs67007@ga.indstate.edu>
2018-05-04 18:27:48 -0400

    att

--- Bipartite_Python_Module/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup, Extension 
-
-bip_module = Extension('bip_module', sources=['bip_py_module.c','bipartite_mod.c'])
-
-setup(name='bip_module',
-      version='0.1',
-      description = 'Check if a graph is Bipartite or not',
-      ext_modules = [bip_module])
-
